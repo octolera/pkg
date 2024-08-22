ef72c34a2a2bf088f0f554cfd787990f5c17d1c0
Astra Linux <astra@astra-monitoring.novalocal>
2024-08-22 17:38:50 +0300

    script with no custom prometheus configs

---
+++ script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+tar xvfz prometheus-*.tar.gz
+# rm prometheus-*.tar.gz
+sudo mkdir /etc/prometheus /var/lib/prometheus
+cd prometheus-2.37.6.linux-amd64
+sudo mv prometheus promtool /usr/local/bin/
+sudo mv prometheus.yml /etc/prometheus/prometheus.yml
+sudo mv consoles/ console_libraries/ /etc/prometheus/
+prometheus --version
+sudo useradd -rs /bin/false prometheus
+sudo chown -R prometheus: /etc/prometheus /var/lib/prometheus
+sudo mv services/prometheus.service /etc/systemd/system/prometheus.service
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+sudo systemctl status prometheus
+
+#nodeexporter
+tar xvfz node_exporter-*.tar.gz
+sudo mv node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin
+sudo useradd -rs /bin/false node_exporter
+sudo mv services/node_exporter.service /etc/systemd/system/node_exporter.service
+sudo systemctl enable node_exporter
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl status node_exporter
+
+#grafana
+sudo dpkg -i *.deb
+sudo systemctl daemon-reload
+sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
