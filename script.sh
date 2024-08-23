a68bcc3ea6cfa7611f64449cfbe4dd21774eed39
octolera <katocatokado@gmail.com>
2024-08-23 09:55:38 +0300

    astra deps script

--- script.sh
+++ script.sh
@@ -9,26 +9,28 @@ sudo mv consoles/ console_libraries/ /etc/prometheus/
 prometheus --version
 sudo useradd -rs /bin/false prometheus
 sudo chown -R prometheus: /etc/prometheus /var/lib/prometheus
+sudo chcon -t bin_t /usr/local/bin/prometheus
 sudo mv services/prometheus.service /etc/systemd/system/prometheus.service
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
-sudo systemctl status prometheus
+sudo systemctl status prometheus | head -n 15
 cd ..
 
 #nodeexporter
 tar xvfz tar/node_exporter-*.tar.gz
 sudo mv node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin
 sudo useradd -rs /bin/false node_exporter
+sudo chcon -t bin_t /usr/local/bin/node_exporter
 sudo mv services/node_exporter.service /etc/systemd/system/node_exporter.service
 sudo systemctl enable node_exporter
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
-sudo systemctl status node_exporter
+sudo systemctl status node_exporter | head -n 15
 
 #grafana
-sudo dpkg -i *.deb
+sudo dpkg -i deb/*
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
-sudo systemctl status grafana-server
+sudo systemctl status grafana-server | head -n 15
