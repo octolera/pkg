5bbea99ec6991b8c125a93e3b8de0f85ce3f8d81
octolera <katocatokado@gmail.com>
2024-08-23 12:18:36 +0300

    astra + dashboards + grafana conf

--- script.sh
+++ script.sh
@@ -3,16 +3,16 @@ tar xvfz tar/prometheus-2.37.6.linux-amd64.tar.gz
 # rm prometheus-*.tar.gz
 sudo mkdir /etc/prometheus /var/lib/prometheus
 cd prometheus-2.37.6.linux-amd64
-sudo mv prometheus promtool /usr/local/bin/
-#sudo mv prometheus.yml /etc/prometheus/prometheus.yml
-sudo mv consoles/ console_libraries/ /etc/prometheus/
+sudo cp prometheus promtool /usr/local/bin/
+#sudo cp prometheus.yml /etc/prometheus/prometheus.yml
+sudo cp consoles/ console_libraries/ /etc/prometheus/
 prometheus --version
 sudo useradd -rs /bin/false prometheus
 sudo chown -R prometheus: /etc/prometheus /var/lib/prometheus
 cd ..
-sudo mv configs/prometheus.yml /etc/prometheus/prometheus.yml
+sudo cp configs/prometheus.yml /etc/prometheus/prometheus.yml
 sudo chcon -t bin_t /usr/local/bin/prometheus
-sudo mv services/prometheus.service /etc/systemd/system/prometheus.service
+sudo cp services/prometheus.service /etc/systemd/system/prometheus.service
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
@@ -20,10 +20,10 @@ sudo systemctl status prometheus | head -n 15
 
 #nodeexporter
 tar xvfz tar/node_exporter-*.tar.gz
-sudo mv node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin
+sudo cp node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin
 sudo useradd -rs /bin/false node_exporter
 sudo chcon -t bin_t /usr/local/bin/node_exporter
-sudo mv services/node_exporter.service /etc/systemd/system/node_exporter.service
+sudo cp services/node_exporter.service /etc/systemd/system/node_exporter.service
 sudo systemctl enable node_exporter
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
@@ -32,6 +32,7 @@ sudo systemctl status node_exporter | head -n 15
 #grafana
 sudo dpkg -i deb/*
 sed -i '0,/"id": .*/{s/"id": .*/"id": null,/}' grafana-dashboards/*.json
+sudo cp configs/grafana.ini /etc/grafana/grafana.ini
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
