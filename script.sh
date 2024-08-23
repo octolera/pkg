9b5b4a064a098dfd6f024fb3a07bd28985df0b0c
octolera <katocatokado@gmail.com>
2024-08-23 14:55:36 +0300

    astra + dashboards + grafana conf

--- script.sh
+++ script.sh
@@ -35,7 +35,7 @@ sed -i '0,/"id": .*/{s/"id": .*/"id": null,/}' grafana-dashboards/*.json
 sudo cp configs/grafana.ini /etc/grafana/grafana.ini
 sudo cp configs/datasources.yaml /etc/grafana/provisioning/datasources/datasources.yml
 sudo cp grafana-dashboards/* /etc/grafana/provisioning/dashboards
-sudo chown grafana /etc/grafana/provisioning/dashboards/ /etc/grafana/provisioning/dashboards/*
+sudo chown grafana /etc/grafana/provisioning/dashboards/*
 sudo systemctl daemon-reload
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
